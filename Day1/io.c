3e26f0206b4f59aad37fc71914f40acc832a8c39
d47sec <datdo.230901@gmail.com>
2021-10-20 22:40:22 +0700

    Day1

---
+++ Day1/io.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int age;
+    char[20] name;
+    printf("Enter your age: \n");
+    scanf("%d", &age);
+    printf("Age of you: %d\n", age);
+    return 0;
+}
---
+++ Day1/variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int x;
+    const double PI = 3.14;
+    printf("Size of int %d\n", sizeof x);
+    printf("%0.2f\n", PI); // format de lay so phan thap phan o trong c 
+    // %f => float
+    // %lf => double 
+    return 0;
+}
